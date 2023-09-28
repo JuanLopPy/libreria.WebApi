7263bff07c39cd572856b0e083681317e8507308
juanm <juanm@JUANMA>
2023-09-28 18:16:19 -0500

    Created properties library

--- libreria.WebApi/Models/Book.cs
+++ libreria.WebApi/Models/Book.cs
@@ -2,5 +2,18 @@
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Autor { get; set; }
+        public DateTime Publication { get; set; }
+        public string Image { get; set; }
+        public string reviw { get; set; }
+
+        public double Rating { get; set; }
+
+        public int UserId { get; set; } 
+
+        public User User { get; set; }
+
     }
 }
