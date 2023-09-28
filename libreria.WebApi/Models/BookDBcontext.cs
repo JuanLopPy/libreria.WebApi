bcdab67a789e37b13c3b3e4db1ab973dd09c13b1
juanm <juanm@JUANMA>
2023-09-28 18:35:51 -0500

    Created Dbcontext

---
+++ libreria.WebApi/Models/BookDBcontext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace libreria.WebApi.Models
+{
+    public class BookDBcontext : DbContext
+    {
+        public BookDBcontext(DbContextOptions<BookDBcontext> options):base (options) { 
+         
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { 
+            modelBuilder.Entity<User>().ToTable("users");
+            modelBuilder.Entity<Book>().ToTable("Books"); 
+        }
+
+    }
+}
